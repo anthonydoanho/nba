453afaf814f147a8f684d737f2640141880a022d
anthonydoanho <anthonydoanho@gmail.com>
2024-05-03 01:01:09 -0700

    Updates to handle multiple draft classes and time.sleep(0.6) to avoid API rate limits

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This is a project to toy with the NBA API. Ideally, I would like to utilize this API to try to evaluate NBA players, possibly through ML solutions.
 
-Initially, I will try to evaluate the 2021 draft class through the info given the API.
+The initial goal of this project will be to examine whether there is any correlation with NBA Combine results with minutes played in the 3 years following the first 3 years of a players'
+career (years 4-6). This coincides with the first 3 years after a player's rookie contract.
+
+Initially, I want to look at draft classes 2014-2019. One year will (2017--chosen somewhat arbitrarily) will be the holdout set.
--- src/nbaPrep.py
+++ src/nbaPrep.py
@@ -1,5 +1,7 @@
 import json
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+import time
 
 from nba_api.stats.endpoints import draftcombinespotshooting 
 from nba_api.stats.endpoints import draftcombinenonstationaryshooting 
@@ -8,64 +10,94 @@ from nba_api.stats.endpoints import draftcombineplayeranthro
 from nba_api.stats.endpoints import leagueleaders
 
 class NBAPrep:
-	def __init__(self, jsonFile):
-		with open(jsonFile, "r") as f:
-			inputs = json.load(f)
-
-		self.target = inputs['target']
-		self.draft = inputs['draftYear']
-		self.years = inputs['seasons']
-		self.measurementCols = inputs['measurementCols']
-		self.spotShootingCols = inputs['spotShootingCols']
-		self.nonStationaryShootingCols = inputs['nonStationaryShootingCols']
+	def __init__(self, target, draftYear, seasons, measurementCols, spotShootingCols, nonStationaryShootingCols):
+		self.target = target
+		self.draft = draftYear
+		self.years = seasons
+		self.measurementCols = measurementCols
+		self.spotShootingCols = spotShootingCols
+		self.nonStationaryShootingCols = nonStationaryShootingCols
 	
 	def players(self):
-		players = pd.DataFrame()
-		for year in self.years:
-			stats = leagueleaders.LeagueLeaders(
-				season=year,
-				season_type_all_star='Regular Season',
-				stat_category_abbreviation='MIN'
-				,per_mode48='Totals'
-			).get_data_frames()[0]
-			players = pd.concat((players, stats)) 
-		playersSum = players.groupby(by='PLAYER_ID', as_index=False).sum()
+		playersSum = pd.DataFrame()
+		for i, draftClass in enumerate(self.years):
+			print('Pulling data for the following years: ' + str(draftClass))
+			players = pd.DataFrame()
+			for year in draftClass:
+				time.sleep(0.6)
+				stats = leagueleaders.LeagueLeaders(
+					season = year,
+					season_type_all_star='Regular Season',
+					stat_category_abbreviation='MIN'
+					,per_mode48='Totals'
+				).get_data_frames()[0]
+				players = pd.concat((players, stats)) 
+			players = players.groupby(by='PLAYER_ID', as_index=False).sum()
+			players['DRAFT_CLASS'] = self.draft[i]
+			playersSum = pd.concat((playersSum, players))
 		
 		return playersSum
 
-	def measurements(self):
-		measurements = draftcombinestats.DraftCombineStats(season_all_time = self.draft).get_data_frames()[0]
-		draftPlayers = measurements[['PLAYER_ID'] + self.measurementCols].sort_values(by='PLAYER_ID', ascending=False)
+	def combine(self):
+		measurements = pd.DataFrame()
+		spotShooting = pd.DataFrame()
+		nonStationaryShooting = pd.DataFrame()
+		for year in self.draft: 
+			print(year)
+			m = draftcombinestats.DraftCombineStats(season_all_time = year).get_data_frames()[0]
+			m['DRAFT_CLASS'] = year
+			time.sleep(0.6)
+			s = draftcombinespotshooting.DraftCombineSpotShooting(season_year = year).get_data_frames()[0]
+			time.sleep(0.6)
+			n = draftcombinenonstationaryshooting.DraftCombineNonStationaryShooting(season_year = self.draft).get_data_frames()[0]
+			time.sleep(0.6)
+			measurements = pd.concat((measurements, m))
+			spotShooting = pd.concat((spotShooting, s))
+			nonStationaryShooting = pd.concat((nonStationaryShooting, n))
+
+		measurements = measurements[measurements['WEIGHT'] != ''] # weight is not blank
+		measurements['WEIGHT'] = measurements['WEIGHT'].astype(float)
+		draftPlayers = measurements[['PLAYER_ID', 'DRAFT_CLASS'] + self.measurementCols].sort_values(by='PLAYER_ID', ascending=False)
 		
-		return draftPlayers
-	
-	def spotShooting(self):
-		spotShooting = draftcombinespotshooting.DraftCombineSpotShooting(season_year = self.draft).get_data_frames()[0]
 		spotShootingTrunc = spotShooting[['PLAYER_ID'] + self.spotShootingCols]
 		spotShootingTrunc = spotShootingTrunc[spotShootingTrunc[self.spotShootingCols].any(axis=1)].sort_values(by='PLAYER_ID', ascending=False)
 
-		return spotShootingTrunc
-
-	def nonStationaryShooting(self):
-		nonStationaryShooting = draftcombinenonstationaryshooting.DraftCombineNonStationaryShooting(season_year = self.draft).get_data_frames()[0]
 		nonStationaryShootingTrunc = nonStationaryShooting[['PLAYER_ID'] + self.nonStationaryShootingCols]
 		nonStationaryShootingTrunc = nonStationaryShootingTrunc[nonStationaryShootingTrunc[self.nonStationaryShootingCols].any(axis=1)].sort_values(by='PLAYER_ID', ascending=False)
-
-		return nonStationaryShootingTrunc
+		
+		return draftPlayers, spotShootingTrunc, nonStationaryShootingTrunc
 
 	def merging(self, playersSum, draftPlayers, spotShootingTrunc, nonStationaryShootingTrunc):
 		df = pd.merge(nonStationaryShootingTrunc, spotShootingTrunc, on='PLAYER_ID', how='outer')
-		df = pd.merge(draftPlayers, df, on='PLAYER_ID', how='inner')
-		df = pd.merge(df, playersSum[['PLAYER_ID', self.target]], on='PLAYER_ID', how='inner').sort_values(by= self.target, ascending=False)
+		df = pd.merge(draftPlayers, df, on='PLAYER_ID', how='outer')
+		df.loc[df['PLAYER_ID']==2006, 'PLAYER_ID'] = 1626204 # Correcting Larry Nance's PLAYER_ID
+		df = pd.merge(df, playersSum[['PLAYER_ID', 'DRAFT_CLASS', self.target]], on=['PLAYER_ID', 'DRAFT_CLASS'], how='left').sort_values(by= self.target, ascending=False)
+
+		df['MIN'] = df['MIN'].fillna(0)
 
 		return df
 
 if __name__ == '__main__':
-	draft = NBAPrep('src/inputs.json')
+	with open('src/inputs.json', "r") as f:
+		inputs = json.load(f)
+
+	draft = NBAPrep(inputs['target'], inputs['draftYear'], inputs['seasons'], inputs['measurementCols'], inputs['spotShootingCols'], inputs['nonStationaryShootingCols'])
+	players = draft.players()
+	measurements, spotShooting, nonStationaryShooting = draft.combine()
+	df = draft.merging(players, measurements, spotShooting, nonStationaryShooting)
+	
+	y_train = df['MIN']
+	X_train = df.drop(['PLAYER_ID', 'MIN', 'FIRST_NAME', 'LAST_NAME'], axis=1)
+	
+	draft = NBAPrep(inputs['target'], inputs['draftYearTest'], inputs['seasonsTest'], inputs['measurementCols'], inputs['spotShootingCols'], inputs['nonStationaryShootingCols'])
 	players = draft.players()
 	measurements = draft.measurements()
 	spotShooting = draft.spotShooting()
 	nonStationaryShooting = draft.nonStationaryShooting()
 	df = draft.merging(players, measurements, spotShooting, nonStationaryShooting)
-
-	import pdb; pdb.set_trace()
+	
+	y_test = df['MIN']
+	X_test = df.drop(['PLAYER_ID', 'MIN', 'FIRST_NAME', 'LAST_NAME'], axis=1)
+	
+	model = LinearRegression(alpha=1e-2).fit(X_train, y_train)
+	model.score(X_test, y_test)
